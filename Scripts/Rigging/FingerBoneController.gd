7c218984e275d24ed00bc7b736961de994959716
baggz-idk <cdubhen@gmail.com>
2026-06-07 13:57:40 -0400

    bug fixesss

--- Scripts/Rigging/FingerBoneController.gd
+++ Scripts/Rigging/FingerBoneController.gd
@@ -1,38 +1,49 @@
 @tool
-class_name FingerBoneController
 extends SkeletonModifier3D
 
 @export var left_controller: XRController3D
 @export var right_controller: XRController3D
 
+@export_group("Smoothing")
+@export_range(0.0, 1.0, 0.01) var smoothing: float = 0.0
+
 @export_group("Left Grip")
 @export var left_grip_bones: Array[FingerBoneConfig] = []
-
 @export_group("Right Grip")
 @export var right_grip_bones: Array[FingerBoneConfig] = []
-
 @export_group("Left Trigger")
 @export var left_trigger_bones: Array[FingerBoneConfig] = []
-
 @export_group("Right Trigger")
 @export var right_trigger_bones: Array[FingerBoneConfig] = []
-
 @export_group("Left Face Buttons")
 @export var left_face_bones: Array[FingerBoneConfig] = []
-
 @export_group("Right Face Buttons")
 @export var right_face_bones: Array[FingerBoneConfig] = []
+
+var _smoothed := {}
 
 func _process_modification() -> void:
 	var skeleton := get_skeleton()
 	if not skeleton:
 		return
-	_apply(left_grip_bones,     left_controller.get_float("grip")    if left_controller  else 0.0, skeleton)
-	_apply(right_grip_bones,    right_controller.get_float("grip")   if right_controller else 0.0, skeleton)
-	_apply(left_trigger_bones,  left_controller.get_float("trigger") if left_controller  else 0.0, skeleton)
-	_apply(right_trigger_bones, right_controller.get_float("trigger")if right_controller else 0.0, skeleton)
-	_apply(left_face_bones,     _face_value(left_controller),  skeleton)
-	_apply(right_face_bones,    _face_value(right_controller), skeleton)
+	var delta := get_process_delta_time()
+
+	_apply(left_grip_bones,     _smooth("lg", left_controller.get_float("grip")    if left_controller  else 0.0, delta), skeleton)
+	_apply(right_grip_bones,    _smooth("rg", right_controller.get_float("grip")   if right_controller else 0.0, delta), skeleton)
+	_apply(left_trigger_bones,  _smooth("lt", left_controller.get_float("trigger") if left_controller  else 0.0, delta), skeleton)
+	_apply(right_trigger_bones, _smooth("rt", right_controller.get_float("trigger")if right_controller else 0.0, delta), skeleton)
+	_apply(left_face_bones,     _smooth("lf", _face_value(left_controller),  delta), skeleton)
+	_apply(right_face_bones,    _smooth("rf", _face_value(right_controller), delta), skeleton)
+
+func _smooth(key: String, target: float, delta: float) -> float:
+	if smoothing <= 0.0:
+		_smoothed[key] = target
+		return target
+	var speed := 1.0 - smoothing
+	var prev: float = _smoothed.get(key, target)
+	var next := prev + (target - prev) * (1.0 - pow(speed, delta * 60.0))
+	_smoothed[key] = next
+	return next
 
 func _apply(configs: Array[FingerBoneConfig], value: float, skeleton: Skeleton3D) -> void:
 	for config in configs:
